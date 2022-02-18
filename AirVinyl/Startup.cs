a96dfeeaa62fbff83b326a23737838f7a70eeed0
adams-y-chen <yuhechen2108@gmail.com>
2022-02-17 21:08:07 -0500

    expand child (navigation) reousrces

--- AirVinyl/Startup.cs
+++ AirVinyl/Startup.cs
@@ -25,6 +25,7 @@ namespace AirVinyl
             services.AddControllers().AddOData(opt =>
                 opt.AddRouteComponents("odata", new AirVinylEntityDataModel().GetEntityDataModel())
                 .Select()
+                .Expand()
             );
 
             services.AddDbContext<AirVinylDbContext>(options =>
